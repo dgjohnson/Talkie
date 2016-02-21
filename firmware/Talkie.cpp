ccc60fd560215950f2ebba1264f7f59ca8aac600
David Johnson <david.g.johnson@outlook.com>
2016-02-21 11:51:42 -0800

    Update Talkie.cpp

--- firmware/Talkie.cpp
+++ firmware/Talkie.cpp
@@ -66,25 +66,6 @@ void Talkie::say(uint8_t* addr) {
 		// Enable the speech system whenever say() is called.
 		
 		pinMode(3,OUTPUT);
-		// Timer 2 set up as a 62500Hz PWM.
-		//
-		// The PWM 'buzz' is well above human hearing range and is
-		// very easy to filter out.
-		//
-		TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-		TCCR2B = _BV(CS20);
-		TIMSK2 = 0;
-	
-		// Unfortunately we can't calculate the next sample every PWM cycle
-		// as the routine is too slow. So use Timer 1 to trigger that.
-		
-		// Timer 1 set up as a 8000Hz sample interrupt
-		TCCR1A = 0;
-		TCCR1B = _BV(WGM12) | _BV(CS10);
-		TCNT1 = 0;
-		OCR1A = F_CPU / FS;
-		TIMSK1 = _BV(OCIE1A);
-
 		setup = 1;
 	}
 
@@ -147,7 +128,6 @@ ISR(TIMER1_COMPA_vect) {
   int16_t u0,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10;
 
   OCR2B = nextPwm;
-  sei();
   if (synthPeriod) {
     // Voiced source
     if (periodCounter < synthPeriod) {
